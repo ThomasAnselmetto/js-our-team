3b702be17c57f208ef2c70568d30369b5073f51b
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-02-06 17:10:46 +0100

    ended esercizi base ora bonus

--- JS/main.js
+++ JS/main.js
@@ -25,54 +25,99 @@
 // Creare l’array di oggetti con le informazioni fornite.
 */
 
+
+const workersCards = document.querySelector(".carta");
+
 const lavoratori = [
 
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        foto: "wayne-barnett-founder-ceo.jpg",
+        foto: "./img/wayne-barnett-founder-ceo.jpg",
 
     },
     {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        foto: "angela-caroll-chief-editor.jpg",
+        foto: "./img/angela-caroll-chief-editor.jpg",
 
     },
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        foto: "walter-gordon-office-manager.jpg",
+        foto: "./img/walter-gordon-office-manager.jpg",
 
     },
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        foto: "angela-lopez-social-media-manager.jpg",
+        foto: "./img/angela-lopez-social-media-manager.jpg",
 
     },
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        foto: "scott-estrada-developer.jpg",
+        foto: "./img/scott-estrada-developer.jpg",
 
     },
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        foto: "barbara-ramos-graphic-designer.jpg",
+        foto: "./img/barbara-ramos-graphic-designer.jpg",
 
     }
 
 ]
 
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
+
+
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+let lavoratore = 0;
 
 for (let lavoratore in lavoratori) {
     console.log(lavoratori[lavoratore]);
 
+
+    workersCards.innerHTML +=
+        `
+    <div class="card" style="width: 18rem;">
+        <img src="./img/wayne-barnett-founder-ceo.jpg" alt="wayne-barnett" class="card-img-top" alt="...">
+            <div class="card-body">
+
+                <p class="card-text">
+                nome:${lavoratori.nome[lavoratore]}
+                ruolo:${lavoratori.ruolo[lavoratore]}
+                </p>
+            </div>
+    </div>
+`
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
